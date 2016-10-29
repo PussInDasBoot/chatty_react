e6a6bafe5f8be04b5124c114a39fa043a1c0f9f5
yemelyn <pommy.shortcake@gmail.com>
2016-10-29 23:01:54 +0000

    Before changing on message function

--- chatty_client/src/App.jsx
+++ chatty_client/src/App.jsx
@@ -2,35 +2,28 @@ import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
-var data = {
-  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
-};
-
 const App = React.createClass({
-  getInitialState: function () {
-    return data;
+  getInitialState: function() {
+    var data = {
+      currentUser: {name: "Bob"},
+      messages: [] // messages coming from the server will be stored here as they arrive
+    };
+    return {data: data};
   },
-  messageID: function () {
-    return this.state.messages.length + 1;
-  },
+  // messageID: function () {
+  //   return this.state.messages.length + 1;
+  // },
   onNewMessage: function(message) {
     this.socket.send(JSON.stringify(message));
   },
   componentDidMount: function () {
     this.socket = new WebSocket("ws://localhost:4000");
     console.log("Connected to server");
+    this.socket.onmessage = function(event) {
+      var msg = JSON.parse(event.data);
+      this.state.data.messages.push(msg);
+      this.setState({data: this.state.data});
+    };
   },
   render: function() {
     return (
@@ -38,8 +31,8 @@ const App = React.createClass({
         <nav>
           <h1>Chatty</h1>
         </nav>
-        <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser} onNewMessage={this.onNewMessage}/>
+        <MessageList messages={this.state.data.messages}/>
+        <ChatBar currentUser={this.state.data.currentUser} onNewMessage={this.onNewMessage}/>
       </div>
     );
   }
--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuid = require('node-uuid');
 
 // Set the port to 4000
 const PORT = 4000;
@@ -18,13 +19,32 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
+
   ws.on('message', function incoming(message) {
     var msg = JSON.parse(message);
     console.log('User %s said %s', msg.username, msg.content);
-  })
+    var message = {
+      id: uuid.v4(),
+      username: msg.username,
+      content: msg.content
+    };
+    return JSON.stringify(message);
+  });
+  ws.on('message', function message(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws) client.send(data);
+    });
+  });
 });
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
